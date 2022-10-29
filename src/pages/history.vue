ece81529c32bae772038e699e1d6e0dc33ed7ae0
nawawa <oonawa12@gmail.com>
2022-10-29 15:01:56 +0900

    経歴ページの下地

---
+++ src/pages/history.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+import { defineComponent, useMeta, useContext } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  head: {},
+  setup(_props, context) {
+    const { $config } = useContext()
+    useMeta({ title: `History - ${$config.siteTitle}` })
+  },
+})
+</script>
